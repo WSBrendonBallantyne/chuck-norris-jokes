dd918ac337b67d520b323ad296973a001a475fe9
WSBrendonBallantyne <brendon@shipwebsource.com>
2022-02-12 09:13:32 -0400

    First commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace wsBrendonBallantyne\ChuckNorrisJokes;
+
+class JokeFactory
+{
+    protected $jokes = [
+        'Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.',
+        'Time waits for no man. Unless that man is Chuck Norris.',
+        'Chuck Norris breathes air … five times a day.',
+        'Jack was nimble, Jack was quick, but Jack still couldn\'t dodge Chuck Norris\' roundhouse kick.'
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+
+    public function hello()
+    {
+        echo 'chuck norris joke goes here';
+    }
+}
